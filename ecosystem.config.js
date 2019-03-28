0c2e6d570511504a008b62208928abc933843f34
maruware <me@maruware.com>
2019-03-28 14:22:53 +0900

    fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,32 @@
 module.exports = {
-  apps : [{
-    name      : 'sample-project',
-    script    : 'index.ts',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: 'sample-project',
+      script: 'dist/index.js',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
-  }],
+  ],
 
-  deploy : {
+  deploy: {
     production: {
-      user : 'sample-user',
-      host : ['123.456.789.0'],
-      key  : '~/.ssh/sample.pem',
-      "ssh_options": ["StrictHostKeyChecking=no", "PasswordAuthentication=no", "ForwardAgent=yes"],
-      ref  : 'origin/master',
-      repo : 'git@github.com:maruware/nodejs-ts-project-sample.git',
-      path : '/var/www/sample-project',
-      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+      user: 'sample-user',
+      host: ['123.456.789.0'],
+      key: '~/.ssh/sample.pem',
+      ssh_options: [
+        'StrictHostKeyChecking=no',
+        'PasswordAuthentication=no',
+        'ForwardAgent=yes'
+      ],
+      ref: 'origin/master',
+      repo: 'git@github.com:maruware/nodejs-ts-project-sample.git',
+      path: '/var/www/sample-project',
+      'post-deploy':
+        'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
     }
   }
-};
+}
