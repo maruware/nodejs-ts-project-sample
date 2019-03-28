9d84445bacf6443120093a9e77a640e83722e7fe
maruware <me@maruware.com>
2019-03-28 16:10:34 +0900

    improve docker file

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,19 @@
 FROM node:10
 
-ENV HOME=/usr/src/app
+ENV PROJECT_ROOT=/usr/src/app
 
 # RUN apt-get install git openssh
 
 # Install app dependencies
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
-COPY package*.json $HOME/
-COPY yarn.lock $HOME/
+COPY package*.json $PROJECT_ROOT/
+COPY yarn.lock $PROJECT_ROOT/
 
-WORKDIR $HOME
+WORKDIR $PROJECT_ROOT
 RUN yarn install
 
 USER root
-COPY . $HOME/
+COPY . $PROJECT_ROOT/
 
 CMD ["yarn", "start"]
--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,16 @@ import log4js from 'log4js'
 import koaLogger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
 
+import auth from 'koa-basic-auth'
+import mount from 'koa-mount'
+import * as swagger from 'swagger2'
+import { ui } from 'swagger2-koa'
+
 import koaError from './middlewares/koa-error'
 import api from './api'
 
 const logger = log4js.getLogger('access')
+const document = swagger.loadDocumentSync('./swagger.yml')
 
 export const setup = () => {
   const app = new Koa()
@@ -21,9 +27,11 @@ export const setup = () => {
   )
   app.use(koaError())
 
-  const server = new http.Server(app.callback())
-
   app.use(api.routes()).use(api.allowedMethods())
 
+  app.use(mount('/docs', auth({ name: 'sample', pass: 'password' })))
+  app.use(ui(document, '/docs'))
+
+  const server = new http.Server(app.callback())
   return server
 }
